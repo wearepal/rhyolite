7d647e97ae91f85abc8e122915ebce4d0e66103a
Thomas M <thomas5max@gmail.com>
2020-09-18 12:09:41 +0100

    Silence some mypy complaints

--- rhyolite/config.py
+++ rhyolite/config.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any, Callable, Optional, Type, List
+from typing import Dict, Any, Callable, Optional, Union, List
 
 
 @dataclass
 class Config:
-    type_hooks: Dict[Type, Callable[[Any], Any]] = field(default_factory=dict)
-    cast: List[Type] = field(default_factory=list)
+    type_hooks: Dict[Union[type, object], Callable[[Any], Any]] = field(default_factory=dict)
+    cast: List[type] = field(default_factory=list)
     forward_references: Optional[Dict[str, Any]] = None
     check_types: bool = True
     strict: bool = False
