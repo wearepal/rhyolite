b1eef478d8851f660011ac2bbb28101fcfa394d7
Thomas M <thomas5max@gmail.com>
2020-09-18 12:13:17 +0100

    Use mapping instead of dict

--- rhyolite/config.py
+++ rhyolite/config.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any, Callable, Optional, Union, List
+from typing import Dict, Any, Callable, Optional, Union, List, Mapping
 
 
 @dataclass
 class Config:
-    type_hooks: Dict[Union[type, object], Callable[[Any], Any]] = field(default_factory=dict)
+    type_hooks: Mapping[Union[type, object], Callable[[Any], Any]] = field(default_factory=dict)
     cast: List[type] = field(default_factory=list)
     forward_references: Optional[Dict[str, Any]] = None
     check_types: bool = True
